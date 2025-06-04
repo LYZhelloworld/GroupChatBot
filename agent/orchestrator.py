51d533a3784b7a5ffb3420150e9f38be6a49108b
LYZhelloworld <1326521+LYZhelloworld@users.noreply.github.com>
2025-06-04 16:56:54 +0800

    Add message hook

--- agent/orchestrator.py
+++ agent/orchestrator.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Callable, Iterable
 
 from agent.agent import Agent
 from agent.history import AgentHistory
@@ -11,14 +11,27 @@ class AgentOrchestrator:
     def __init__(self, agents: Iterable[Agent], history: AgentHistory):
         self.__agents = list(agents)
         self.__history = history
+        self.__chat_message_hook: Callable[[str, str], None] = lambda name, message: None
+
+    @property
+    def chat_message_hook(self) -> Callable[[str, str], None]:
+        return self.__chat_message_hook
+
+    @chat_message_hook.setter
+    def chat_message_hook(self, hook: Callable[[str, str], None]):
+        self.__chat_message_hook = hook
 
     def chat(self, message: str):
         self.__history.add(USER_AGENT_NAME, message)
+        self.__chat_message_hook(USER_AGENT_NAME, message)
 
         while True:
             history_length = len(self.__history.history)
             for agent in self.__agents:
                 agent.receive_message()
+                if len(self.__history.history) > history_length:
+                    last_message = self.__history.history[-1]
+                    self.__chat_message_hook(last_message.name, last_message.message)
             if len(self.__history.history) == history_length:
                 # No new messages, stop
                 break
--- agent/tools.py
+++ agent/tools.py
@@ -13,13 +13,13 @@ user_dict: dict[UserName, UserData] = {}
 
 @tool
 def get_user_list() -> list[str]:
-    '''获取所有用户的名称列表'''
+    '''获取所角色的名称列表'''
     return list(user_dict.keys())
 
 
 @tool
 def get_user_instructions(name: str) -> str:
-    '''获取指定用户的设定'''
+    '''获取指定角色的设定'''
     return user_dict.get(name, UserData(instructions='')).instructions
 
 
